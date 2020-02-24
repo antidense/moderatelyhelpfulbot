44669d3514f7569b2feb20b2d17d61d792ec48f6
antidense <61423286+antidense@users.noreply.github.com>
2020-02-24 11:21:57 -0500

    removed sensitive data

--- ModeratelyHelpfulBot.py
+++ ModeratelyHelpfulBot.py
@@ -16,12 +16,12 @@ from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import pprint
-from settings import bot_name, password, client_id, client_secret
+from settings import bot_name, password, client_id, client_secret, BOT_OWNER, db_engine
 
-BOT_OWNER = "antidense"
+
 
 # Set up database
-db_engine = '***REMOVED***'
+
 engine = create_engine(db_engine)
 Base = declarative_base(bind=engine)
 
