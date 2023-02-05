4b06a020227cd65b047bf59740ead5862494e72b
antidense <ntkurapati+ad@gmail.com>
2023-02-05 07:09:12 -0500

    fix config_last_checked null

--- utils.py
+++ utils.py
@@ -1347,7 +1347,7 @@ def get_subreddit_by_name(wd: WorkingData, subreddit_name: str, create_if_not_ex
             return None
 
     # Update from scratch if it has been a while
-    if tr_sub.config_last_checked < datetime.now() - timedelta(hours=SUBWIKI_CHECK_INTERVAL_HRS):
+    if not tr_sub.config_last_checked or tr_sub.config_last_checked < datetime.now() - timedelta(hours=SUBWIKI_CHECK_INTERVAL_HRS):
         print(f"GSBN: needs update {tr_sub.subreddit_name}")
         sub_info = wd.ri.get_subreddit_info(subreddit_name=tr_sub.subreddit_name)
 
